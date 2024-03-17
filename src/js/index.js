cf57e7f180751a2df2db5caff44746b93d3797fe
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-17 14:12:49 -0400

    new todo function working

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,22 @@ import { Todo } from './todo';
 import { Project } from './projects';
 
 const todos = [];
+const projects = [new Project('Default', 'This is the default project')];
+
+let test = new Project('Test', 'This is a test project');
+projects.push(test);
+
+createTodo('Test', 1);
+
 function createTodo() {
-  const newTodo = new Todo(arguments);
+  const newTodo = new Todo(...arguments);
   todos.push(newTodo);
+  projects[0].addTodo(newTodo);
+  if (newTodo.project <= projects.length - 1) {
+    if (newTodo.project) {
+      projects[newTodo.project].todos.push(newTodo);
+    }
+  }
 }
+
+console.log(projects);
--- src/js/todo.js
+++ src/js/todo.js
@@ -2,9 +2,8 @@
 class Todo {
   constructor() {
     this.title = arguments[0];
-    this.project = 'default';
     if (arguments.length > 1) {
-      this.description = arguments[1];
+      this.project = arguments[1];
     }
     if (arguments.length > 2) {
       this.dueDate = arguments[2];
@@ -13,7 +12,7 @@ class Todo {
       this.priority = arguments[3];
     }
     if (arguments.length > 4) {
-      this.project = arguments[4];
+      this.description = arguments[4];
     }
   }
 }

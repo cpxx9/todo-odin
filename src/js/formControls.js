910aa0a9e7d7f007275ba17eda6bd002ba2c31f9
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-18 21:56:11 -0400

    form now clears when hidden

--- src/js/formControls.js
+++ src/js/formControls.js
@@ -12,8 +12,17 @@ import { loadCards, loadProjects, currentLoadedProject } from './domController';
 
 function formControls(todo) {
   const editFormWrapper = document.querySelector('#editFormWrapper');
+  const editForm = document.querySelector('#editForm');
   editFormWrapper.classList.add('form-open');
-  console.log(todo);
+
+  const hideBtn = document.querySelector('#hideBtn');
+  hideBtn.addEventListener('click', function () {
+    editFormWrapper.classList.remove('form-open');
+    setInterval(resetForm, 500);
+    function resetForm() {
+      editForm.reset();
+    }
+  });
 }
 
 export { formControls };
